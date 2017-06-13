1e005076cf262bdb4d2f4298bf23f95a8707b1af
Michael Joseph Russo <michaeljosephrusso@Michaels-MacBook-Air.local>
2017-06-12 21:54:10 -0400

    added articles page and updated minor hotels page issue

--- assets/javascript/hotels.js
+++ assets/javascript/hotels.js
@@ -23,159 +23,159 @@ since it is called inside another loop.*/
 //count used to limit results to 10
 
 $(document).ready(function(){
-// on page load the city is retrieved from the localStorage and function getCityID is ran.
-var city = localStorage.getItem("city Search");
-getCityID(city);
-//at the bottom of this file, there's an event listener for a form submit that gets the input from the text-area of the form.
-var count = 0;
-var createRow = "";
-var createCol ="";
-var name = "";
+	// on page load the city is retrieved from the localStorage and function getCityID is ran.
+	var city = localStorage.getItem("city Search");
+	getCityID(city);
+	//at the bottom of this file, there's an event listener for a form submit that gets the input from the text-area of the form.
+	var count = 0;
+	var createRow = "";
+	var createCol ="";
+	var name = "";
 
-function moreVenues(vId, counter){
+	function moreVenues(vId, counter){
 
-	var queryURL = "https://api.tripexpert.com/v1/venues/"+vId+"?&api_key=16f4b9a0eaabb835e60aa42e89c48e11";
+		var queryURL = "https://api.tripexpert.com/v1/venues/"+vId+"?&api_key=16f4b9a0eaabb835e60aa42e89c48e11";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(data){
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(data){
 
-		var path = data.response.venues[0];
+			var path = data.response.venues[0];
 
-		//console.log(data);
-		//name of attraction
-		console.log(path.name);	
-		console.log("HERE! " + count);
-		$("#column"+(counter-1)).append("<h2>"+path.name+"</h2>");
-		$("#column"+(counter-1)).append("<br>");
-		//address					
-		console.log(path.address);
-		$("#column"+(counter-1)).append(path.address);
-		$("#column"+(counter-1)).append("<br>");
-		//open hours
-		// console.log(path.opening_hours);
-		// $("#column"+(counter-1)).append(path.opening_hours);
-		// $("#column"+(counter-1)).append("<br>");
-		//review 1
-		console.log("Review 1: " + path.reviews[0].extract);
-		$("#column"+(counter-1)).append("<b>Review: </b>"+path.reviews[0].extract);
-		$("#column"+(counter-1)).append("<br>");
-		//review 2
-		// console.log("Review 2: " + path.reviews[1].extract);
-		// $("#column").append("<b>Review: </b>"+path.reviews[1].extract);
-		// $("#column").append("<br>");
-		//phone number
-		console.log(path.telephone);
-		if(path.telephone!=null){
-			$("#column"+(counter-1)).append(path.telephone);
+			//console.log(data);
+			//name of attraction
+			console.log(path.name);	
+			console.log("HERE! " + count);
+			$("#column"+(counter-1)).append("<h2>"+path.name+"</h2>");
 			$("#column"+(counter-1)).append("<br>");
-		}
-		//website
-		console.log(path.website);
-		$("#column"+(counter-1)).append("<a id='link' href='" + path.website + "'>More Info</a>");
-		$("#column"+(counter-1)).append("<br><br>");
-	
-	});
+			//address					
+			console.log(path.address);
+			$("#column"+(counter-1)).append(path.address);
+			$("#column"+(counter-1)).append("<br>");
+			//open hours
+			// console.log(path.opening_hours);
+			// $("#column"+(counter-1)).append(path.opening_hours);
+			// $("#column"+(counter-1)).append("<br>");
+			//review 1
+			console.log("Review 1: " + path.reviews[0].extract);
+			$("#column"+(counter-1)).append("<b>Review: </b>"+path.reviews[0].extract);
+			$("#column"+(counter-1)).append("<br>");
+			//review 2
+			// console.log("Review 2: " + path.reviews[1].extract);
+			// $("#column").append("<b>Review: </b>"+path.reviews[1].extract);
+			// $("#column").append("<br>");
+			//phone number
+			console.log(path.telephone);
+			if(path.telephone!=null){
+				$("#column"+(counter-1)).append(path.telephone);
+				$("#column"+(counter-1)).append("<br>");
+			}
+			//website
+			console.log(path.website);
+			$("#column"+(counter-1)).append("<a id='link' href='" + path.website + "'>More Info</a>");
+			$("#column"+(counter-1)).append("<br><br>");
+		
+		});
 
-}
+	}
 
-function searchByDestId(dId){
-	
-	var queryURL = "https://api.tripexpert.com/v1/venues?destination_id=" + dId +"&api_key=16f4b9a0eaabb835e60aa42e89c48e11";
+	function searchByDestId(dId){
+		
+		var queryURL = "https://api.tripexpert.com/v1/venues?destination_id=" + dId +"&api_key=16f4b9a0eaabb835e60aa42e89c48e11";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(data){
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(data){
 
-		var path = data.response;
-	
+			var path = data.response;
+		
 
-		console.log(data);
+			console.log(data);
 
-		// var createRow = $("<div class='row' id='row"+count+"'></div>");
+			// var createRow = $("<div class='row' id='row"+count+"'></div>");
 
-		// $("#main").append(createRow);
+			// $("#main").append(createRow);
 
-		// var createCol = $("<div class='col s6' id='column'></div>");
+			// var createCol = $("<div class='col s6' id='column'></div>");
 
-		// $("#row"+count).append(createCol);
+			// $("#row"+count).append(createCol);
 
-		//traverses entire venues array for all entries of type "attraction" 
-		for(var i = 0; i < path.venues.length; i++){
+			//traverses entire venues array for all entries of type "attraction" 
+			for(var i = 0; i < path.venues.length; i++){
+
+				
+
+				if(path.venues[i].venue_type_id == 1 && count<10){
+
+					createRow = $("<div class='row' id='row"+count+"'></div>");
+
+					$("#main").append(createRow);
+
+					createCol = $("<div class='col s6' id='column"+count+"'></div>");
+
+					$("#row"+count).append(createCol);
+					//count used to make sure we only get the top ten results
+					count++;
+
+					//console.log("HERE: " +path.venues[i].id);
+
+					//call this function if the id=3 aka is an attraction 
+					moreVenues(path.venues[i].id,count);
+					
+				}
+
+			}
+			count=0;
+			//console.log(data);
+			
+		});
+
+	}
+
+	//this function uses the API to extract the ID of the city
+	function getCityID(city){
+
+		var queryURL = "https://api.tripexpert.com/v1/destinations?&api_key=16f4b9a0eaabb835e60aa42e89c48e11";
+		var id = "";
+		var name = city;
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(data){
 
 			
+			
+			/*This for loop goes into the api objects array and matches the city from the input
+			and then saves the ID number from that location in the object array*/
 
-			if(path.venues[i].venue_type_id == 1 && count<10){
+			for(var i = 0; i<data.response.venues.length;i++){
 
-				createRow = $("<div class='row' id='row"+count+"'></div>");
+				if(data.response.venues[i].name.toLowerCase() == city.toLowerCase()){
+					//this is where the city ID is stored
+					id = data.response.venues[i].id;
+					//console.log("ID: " + id);
+				}
 
-				$("#main").append(createRow);
-
-				createCol = $("<div class='col s6' id='column"+count+"'></div>");
-
-				$("#row"+count).append(createCol);
-				//count used to make sure we only get the top ten results
-				count++;
-
-				//console.log("HERE: " +path.venues[i].id);
-
-				//call this function if the id=3 aka is an attraction 
-				moreVenues(path.venues[i].id,count);
-				
 			}
-
-		}
-		count=0;
-		//console.log(data);
-		
-	});
-
-}
-
-//this function uses the API to extract the ID of the city
-function getCityID(city){
-
-	var queryURL = "https://api.tripexpert.com/v1/destinations?&api_key=16f4b9a0eaabb835e60aa42e89c48e11";
-	var id = "";
-	var name = city;
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(data){
-
-		
-		
-		/*This for loop goes into the api objects array and matches the city from the input
-		and then saves the ID number from that location in the object array*/
-
-		for(var i = 0; i<data.response.venues.length;i++){
-
-			if(data.response.venues[i].name.toLowerCase() == city.toLowerCase()){
-				//this is where the city ID is stored
-				id = data.response.venues[i].id;
-				//console.log("ID: " + id);
-			}
-
-		}
-		//takes the id we found and passes it to the searchById function
-		searchByDestId(id);
-
-	});
-
-}
-
-//gets the input from the search bar 
-		$("#search").on("click", function(event){
-			//$("#column").remove();
-			//$("#link").remove();
-			//$(".row").remove();
-			$("#main").html("");
-			event.preventDefault();
-			var city = $("#city-input").val().trim();
-			getCityID(city);
+			//takes the id we found and passes it to the searchById function
+			searchByDestId(id);
 
 		});
 
-})
+	}
+
+	//gets the input from the search bar 
+	$("#search").on("click", function(event){
+		//$("#column").remove();
+		//$("#link").remove();
+		//$(".row").remove();
+		$("#main").html("");
+		event.preventDefault();
+		var city = $("#city-input").val().trim();
+		getCityID(city);
+
+	});
+
+});
